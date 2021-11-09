6b7f060fca12fe2a5a92dc8301763a00ac8e4584
MIHIR AGRAWAL <agarwalmihir08@gmail.com>
2021-11-10 00:00:24 +0530

    array

---
+++ Array/Print elements in sorted order using row-column wise sorted matrix.cpp
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<vector<int>> sortedMatrix(int N, vector<vector<int>> Mat) {
+        vector<int>v;
+        //store in vector
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                v.push_back(Mat[i][j]);
+            }
+        }
+        //sort with vector
+        sort(v.begin(),v.end());
+        int index = 0;
+        
+        //populate the matrix
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                Mat[i][j] = v[index];
+                index++;
+            }
+        }
+        
+        return Mat;
+    }
+};
+
+// { Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<vector<int>> v(N, vector<int>(N));
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++) cin >> v[i][j];
+        Solution ob;
+        v = ob.sortedMatrix(N, v);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) cout << v[i][j] << " ";
+            cout << "\n";
+        }
+    }
+}  // } Driver Code Ends
