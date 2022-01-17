b1e90be02e74dc7103c38ec0ba095f328601c032
MIHIR AGRAWAL <agarwalmihir08@gmail.com>
2022-01-17 23:37:48 +0530

    Update and Bit Manipulation

--- String/Rabin Karp Algo.cpp
+++ String/Rabin Karp Algo.cpp
@@ -9,29 +9,29 @@ using namespace std;
         vector <int> search(string pat, string s)
         {
             vector<int> ans;
-    vector<long long> powers(1e5+7,0);
-    powers[0]=1;
-    for(int i=1;i<1e5+7;i++)
-    {
-        powers[i]=(powers[i-1]*27)%1000000007;
-    }
-    vector<long long> hash(s.size()+1,0);
-    for(int i=0;i<s.size();i++)
-    {
-        hash[i+1]=(hash[i]+((s[i]-'a'+1)*powers[i]))%1000000007;
-    }
-    long long temp=0;
-    for(int i=0;i<pat.length();i++)
-    {
-        temp=(temp+((pat[i]-'a'+1)*powers[i]))%1000000007;
-    }   
-    for(int i=0;i+pat.length()-1<s.length();i++)
-    {
-        long long cur=0;
-        cur=(hash[i+pat.length()]-hash[i]+1000000007)%1000000007;
-        if(cur==(temp*powers[i])%1000000007)
-            ans.push_back(i+1);
-    }
+            vector<long long> powers(1e5+7,0);
+            powers[0]=1;
+            for(int i=1;i<1e5+7;i++)
+            {
+                powers[i]=(powers[i-1]*27)%1000000007;
+            }
+            vector<long long> hash(s.size()+1,0);
+            for(int i=0;i<s.size();i++)
+            {
+                hash[i+1]=(hash[i]+((s[i]-'a'+1)*powers[i]))%1000000007;
+            }
+            long long temp=0;
+            for(int i=0;i<pat.length();i++)
+            {
+                temp=(temp+((pat[i]-'a'+1)*powers[i]))%1000000007;
+            }   
+            for(int i=0;i+pat.length()-1<s.length();i++)
+            {
+                long long cur=0;
+                cur=(hash[i+pat.length()]-hash[i]+1000000007)%1000000007;
+                if(cur==(temp*powers[i])%1000000007)
+                    ans.push_back(i+1);
+            }
             if(ans.empty())
             {
                 ans.push_back(-1);
